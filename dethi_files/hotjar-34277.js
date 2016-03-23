a6ab6e1220c8fd5c6996712c87d8eb63dca1f3e9
gauxo <duynhat.cse@gmail.com>
2016-03-23 19:29:31 +0700

    Initial

---
+++ dethi_files/hotjar-34277.js
@@ -0,0 +1,9 @@
+window.hjSiteSettings = {"testers_widgets":[],"surveys":[],"heatmaps":[],"recording_capture_keystrokes":true,"polls":[],"site_id":34277,"record_targeting_rules":[],"forms":[],"record":false,"r":1.0,"deferred_page_contents":[]};
+
+window.hjBootstrap = function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+};
+
+hjBootstrap('https://script.hotjar.com/modules-043c1e6abe660c48857202e419ff9d8a.js');
